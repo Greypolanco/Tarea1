a23368e298699d8b3e52d893fa70572a0f854ce7
greypolanco <greilyntejada03@gmail.com>
2023-09-04 23:12:17 -0400

    agregando data base

---
+++ app/src/main/java/com/example/firstaplicacion/data/local/DataBase.kt
@@ -0,0 +1,15 @@
+package com.example.firstaplicacion.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.firstaplicacion.data.local.dao.UsuarioDao
+import com.example.firstaplicacion.data.local.entity.Usuario
+
+@Database(
+    entities = [Usuario::class],
+    exportSchema = false,
+    version = 1
+)
+abstract class DataBase: RoomDatabase()  {
+    abstract val userdao: UsuarioDao
+}
